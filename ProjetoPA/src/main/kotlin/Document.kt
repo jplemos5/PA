fa287d0215c814cd781822eeb2c5aa389fe8ed42
jplemos5 <jpsfa3@iscte-iul.pt>
2024-04-15 13:08:02 +0100

    Documentation in English

--- ProjetoPA/src/main/kotlin/Document.kt
+++ ProjetoPA/src/main/kotlin/Document.kt
@@ -1,25 +1,57 @@
 import java.io.File
 import java.lang.StringBuilder
 
+/**
+ * Represents an XML document with a root entity, version, and encoding.
+ * @param name The name of the document.
+ * @property version The version of the document.
+ * @property encoding The encoding of the document.
+ * @property entity The Root Entity of the document.
+ */
 class Document(name: String, private var version: String, private var encoding: String) {
+    // Root entity of the document
     private val entity: Entity = Entity(name)
 
+    /**
+     * Gets the version of the document.
+     * @return The version of the document.
+     */
     fun getVersion() : String = version
 
+    /**
+     * Gets the encoding of the document.
+     * @return The encoding of the document.
+     */
     fun getEncoding() : String = encoding
 
-    fun setVersion(version: String) {
-        this.version = version
-    }
+    /**
+     * Sets the version of the document.
+     * @param version The new version for the document.
+     */
+    fun setVersion(version: String) { this.version = version }
 
-    fun setEncoding(encoding: String) {
-        this.encoding = encoding
-    }
+    /**
+     * Sets the encoding of the document.
+     * @param encoding The new encoding for the document.
+     */
+    fun setEncoding(encoding: String) { this.encoding = encoding }
 
+    /**
+     * Gets the root entity of the document.
+     * @return The root entity of the document.
+     */
     fun getRootEntity(): Entity = entity
 
+    /**
+     * Generates a pretty-printed string representation of the document.
+     * @return A pretty-printed string representation of the document.
+     */
     fun prettyPrint(): String = StringBuilder("<?xml version=\"$version\" encoding=\"$encoding\"?>\n" + entity.prettyPrint()).toString()
 
+    /**
+     * Writes the document content to a file with the specified filename.
+     * @param fileName The name of the file to write to.
+     */
     fun writeToFile(fileName: String) = File(fileName).writeText(prettyPrint())
 
 }
--- ProjetoPA/src/main/kotlin/Entity.kt
+++ ProjetoPA/src/main/kotlin/Entity.kt
@@ -1,69 +1,171 @@
+/**
+ * Represents an entity with a name, attributes, parent, and children.
+ *
+ * @constructor Creates an Entity
+ * @property name The name of the entity.
+ * @property attributes The attributes of the entity as a mutable map.
+ * @property parent The parent entity of the current entity, or null if it has no parent.
+ * @property children The list of child entities of the current entity.
+ */
 class Entity(private var name: String, private var attributes: MutableMap<String, String?> = mutableMapOf() , private var parent: Entity? = null ) {
 
     private var children: MutableList<Entity> = mutableListOf()
 
+    /**
+     * Gets the name of the entity.
+     * @return The name of the entity.
+     */
     fun getName(): String = this.name
 
+    /**
+     * Gets the attributes of the entity.
+     * @return The attributes of the entity as a mutable map.
+     */
     fun getAttributes(): MutableMap<String, String?> = this.attributes
 
+    /**
+     * Gets the children of the entity.
+     * @return The children of the entity as a mutable list.
+     */
     fun getChildren(): MutableList<Entity> = this.children
 
+    /**
+     * Gets the parent of the entity.
+     * @return The parent of the entity, or null if the entity has no parent.
+     */
     fun getParent(): Entity? = this.parent
 
+    /**
+     * Gets the text content of the entity.
+     * If the entity has only one attribute which is an empty string, returns the attribute name.
+     * Otherwise, returns "Doesn't have text!".
+     * @return The text content of the entity.
+     */
     fun getText() : String {
         if(attributes.size == 1 && attributes.values.first() === "") return attributes.keys.first()
         return "Doesn't have text!"
     }
+
+    /**
+     * Sets the name of the entity.
+     * @param name The new name for the entity.
+     */
     private fun setName(name: String){
         this.name = name
     }
 
+    /**
+     * Sets the parent of the entity.
+     * @param parent The parent entity to set.
+     */
     private fun setParent(parent: Entity?){
         this.parent = parent
     }
 
+    /**
+     * Adds an attribute to the entity.
+     * @param attributeName The name of the attribute to add.
+     * @param attributeValue The value of the attribute to add.
+     * @return The previous value associated with [attributeName], or null if there was no mapping for [attributeName].
+     */
     fun addAttribute(attributeName: String, attributeValue: String?) = attributes.put(attributeName, attributeValue)
 
+    /**
+     * Removes the attribute with the specified name from the entity.
+     * @param attribute The name of the attribute to remove.
+     * @return The previous value associated with [attribute], or null if there was no mapping for [attribute].
+     */
     fun removeAttribute(attribute : String) = attributes.remove(attribute)
 
+    /**
+     * Changes the value of an existing attribute in the entity.
+     * If the attribute doesn't exist, it does nothing.
+     * @param attribute The name of the attribute to change.
+     * @param value The new value of the attribute.
+     * @return The previous value associated with [attribute], or null if there was no mapping for [attribute].
+     */
     fun changeAttribute(attribute : String, value : String) = attributes.replace(attribute, value)
 
+    /**
+     * Changes the name of an existing attribute in the entity.
+     * If the attribute doesn't exist, it does nothing.
+     * @param oldName The current name of the attribute.
+     * @param newName The new name to set for the attribute.
+     */
     private fun changeAttributeName(oldName : String, newName : String){
         val value = attributes.remove(oldName)// TODO saber se vale a pena tentar manter a ordem
         addAttribute(newName, value)
     }
 
+    /**
+     * Adds a child entity to the current entity.
+     * @param entity The entity to add as a child.
+     */
     fun addChildEntity(entity: Entity) {
         children.add(entity)
         entity.setParent(this)
     }
 
+    /**
+     * Removes a child entity from the current entity.
+     * @param entity The entity to remove from the children.
+     */
     fun removeChildEntity(entity: Entity) {
         entity.setParent(null)
         children.remove(entity)
     }
 
-    private fun attributesAreBlank() =  attributes.all { it.value.isNullOrBlank() }
+    /**
+     * Checks if all attributes in the entity are blank or null.
+     * @return true if all attributes are blank or null, false otherwise.
+     */
+    private fun attributesAreBlank() : Boolean =  attributes.all { it.value.isNullOrBlank() }
 
+    /**
+     * Returns a string representation of the entity.
+     * The string includes the name, attributes, parent entity (if any), and children entities.
+     * @return A string representation of the entity.
+     */
     override fun toString(): String {
         return "Name: $name, Attributes: $attributes, Parent: ${parent?.name ?: "null"}, Children: $children"
     }
 
+    /**
+     * Represents a visitor interface for entities.
+     */
     interface Visitor {
+        /**
+         * Visits an entity.
+         * @param entity The entity to visit.
+         */
         fun visit(entity: Entity)
     }
 
+    /**
+     * Creates a visitor from the provided function.
+     * @param visitor The function that defines the visit behavior.
+     * @return A visitor based on the provided function.
+     */
     private fun visitor(visitor: (Entity) -> Unit) = object : Visitor {
         override fun visit(entity: Entity) {
             visitor(entity)
         }
     }
 
+    /**
+     * Accepts a visitor and visits the current entity and its children.
+     * @param visitor The visitor to accept.
+     */
     private fun accept(visitor: Visitor){
         visitor.visit(this)
         children.forEach{ it.accept(visitor)}
     }
 
+    /**
+     * Generates a pretty-printed XML representation of the entity and its children.
+     * @param indentation The indentation level for formatting (default is 0).
+     * @return A pretty-printed XML representation of the entity and its children.
+     */
     fun prettyPrint(indentation: Int = 0): String {
         val indent = "    ".repeat(indentation)
         val stringBuilder = StringBuilder("$indent<$name").apply {
@@ -74,6 +176,11 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         return stringBuilder.toString()
     }
 
+    /**
+     * Appends attributes to the given StringBuilder.
+     * @param stringBuilder The StringBuilder to append attributes to.
+     * @return The StringBuilder with attributes appended.
+     */
     private fun placeAttributes(stringBuilder: StringBuilder) : StringBuilder {
         if (attributes.isNotEmpty()) {
             attributes.forEach { (key, value) -> stringBuilder.append(if (value.isNullOrBlank()) key else " $key=\"$value\"") }
@@ -82,6 +189,13 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         return stringBuilder
     }
 
+    /**
+     * Recursively traverses through children and appends their pretty-printed representations to the StringBuilder.
+     * @param stringBuilder The StringBuilder to append child representations to.
+     * @param indentation The current indentation level.
+     * @param indent The string representation of indentation.
+     * @return The StringBuilder with child representations appended.
+     */
     private fun goThroughChildren(stringBuilder: StringBuilder, indentation: Int, indent:String) : StringBuilder{
         if (children.isNotEmpty()) {
             children.forEach { child -> stringBuilder.append("\n" + child.prettyPrint(indentation + 1))}
@@ -90,21 +204,35 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         return stringBuilder
     }
 
+    /**
+     * Prints the name and attributes of the entity and its children.
+     */
     fun attributePrinter() =
         accept(visitor { println("Entity: $name, Attributes: $attributes") })
 
+    /**
+     * Prints the name of the entity and its children.
+     */
     fun namePrinter() =
         accept(visitor { println("Entity Name: $name") })
 
+    /**
+     * Retrieves a list of names of all entities in the hierarchy.
+     * @return A list containing the names of all entities.
+     */
     fun entityList(): MutableList<String> {
         val resultList = mutableListOf<String>()
-        val v = visitor {
-                resultList.add(it.getName())
-            }
+        val v = visitor { resultList.add(it.getName()) }
         accept(v)
         return resultList
     }
 
+    /**
+     * Adds an attribute to all entities with the specified name, if they don't have any non-blank attributes.
+     * @param entityName The name of the entities to which the attribute will be added.
+     * @param attributeName The name of the attribute to add.
+     * @param attributeValue The value of the attribute to add.
+     */
     fun globalAddAttributeToEntity(entityName: String,  attributeName: String,  attributeValue: String){
         val v = visitor { entity ->
             if (entity.name == entityName && entity.attributes.none { it.value.isNullOrBlank() })
@@ -113,6 +241,11 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         accept(v)
     }
 
+    /**
+     * Renames an entity with the specified old name to the new name globally throughout the hierarchy.
+     * @param entityOldName The current name of the entity to rename.
+     * @param entityNewName The new name for the entity.
+     */
     fun globalRenameEntity(entityOldName: String,  entityNewName: String){
         if(this.name != entityNewName) { //TODO Perguntar ao stor se vale a pena ou n. E no adicionar Entidade?
             val v = visitor { entity ->
@@ -123,6 +256,12 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         }
     }
 
+    /**
+     * Renames a specified attribute of entities with the given name throughout the hierarchy.
+     * @param entityName The name of the entities whose attributes will be renamed.
+     * @param attributeOldName The current name of the attribute to rename.
+     * @param attributeNewName The new name for the attribute.
+     */
     fun globalRenameAttribute(entityName: String,  attributeOldName: String,  attributeNewName: String){
         val v = visitor { entity ->
             if (entity.name == entityName && entity.attributes.contains(attributeOldName))
@@ -131,6 +270,10 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         accept(v)
     }
 
+    /**
+     * Removes all entities with the specified name from the hierarchy except for the root entity.
+     * @param entityName The name of the entities to remove.
+     */
     fun globalRemoveEntity(entityName: String) {
         if (this.name != entityName) { //Nao pode remover a root
             val v = visitor { entity ->
@@ -144,6 +287,11 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         }
     }
 
+    /**
+     * Removes a specified attribute from entities with the given name throughout the hierarchy.
+     * @param entityName The name of the entities from which the attribute will be removed.
+     * @param attributeName The name of the attribute to remove.
+     */
     fun globalRemoveAttribute(entityName: String, attributeName : String){
         val v = visitor { entity ->
             if(entity.name == entityName && entity.attributes.contains(attributeName) && !entity.attributesAreBlank())
@@ -152,6 +300,11 @@ class Entity(private var name: String, private var attributes: MutableMap<String
         accept(v)
     }
 
+    /**
+     * Performs a global XPath search on the hierarchy and returns a string containing the XML representation of matching entities.
+     * @param path The XPath expression to search for.
+     * @return A string containing the XML representation of entities matching the XPath expression.
+     */
     fun globalXPath(path: String) : String {
         val listToSearch : MutableList<String> = path.split("/") as MutableList<String>
         var str = ""
